45a0f1e26026077620daa801ca349c3eb07b0293
kiralybalint <kiraly.balint@simonyi.bme.hu>
2018-10-06 11:46:40 +0200

    List active and inactive users on team page

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,9 @@
 module TeamsHelper
+  def active_members(team)
+    @active_members = team.memberships.select(&:active?)
+  end
+
+  def inactive_members(team)
+    @inactive_members = team.memberships.select { |m| !m.active? }
+  end
 end
