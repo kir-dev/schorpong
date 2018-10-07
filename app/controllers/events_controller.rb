2d134bedccdf9840ece1df021241cb4be670686a
kiralybalint <kiraly.balint@simonyi.bme.hu>
2018-10-07 22:15:10 +0200

    Fix events order

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.order(created_at: :desc).all
   end
 
   def show
