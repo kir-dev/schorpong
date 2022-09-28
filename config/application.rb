4671d5f3aa4e4f7bcd68b55da9376bcf0105fac7
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-09-28 21:12:59 +0200

    add GH Action to run tests

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Schorpong
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2
     config.i18n.default_locale = :hu
 
     # Configuration for the application, engines, and railties goes here.
