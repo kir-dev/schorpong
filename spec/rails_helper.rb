2f70ed0fbe589c8ffb0abcab7b4a806cb9e7bd5d
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-09-29 21:55:37 +0200

    add factory_bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,18 +1,16 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-#
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-if ENV['RAILS_ENV'] == 'test'
-  require 'simplecov'
-  SimpleCov.start 'rails'
-  puts "required simplecov"
-end
+
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require_relative './support/factory_bot'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
