2d5d4e388efa9a3c2c6d3fb44682997f7544cd75
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-09-30 15:14:01 +0200

    add test for RentNumberValidator

--- spec/models/rent_spec.rb
+++ spec/models/rent_spec.rb
@@ -3,11 +3,24 @@ require 'rails_helper'
 RSpec.describe Rent, type: :model do
   let(:user) { create(:user) }
   let(:item) { create(:item) }
+  let(:pre_existing_rent) do
+    Rent.create!(state: :approved, item: item, user: user,
+                 begin: DateTime.new(2022, 1, 10),
+                 end: DateTime.new(2022, 1, 15),
+                 number: 5)
+  end
+  let(:current_rent) do
+    Rent.new(state: :approved, item: item, user: user,
+             begin: begin_date,
+             end: end_date,
+             number: 5)
+  end
 
-  subject { Rent.new(state: :approved, item: item, user: user,
-                         begin: DateTime.new(2022, 1, 10),
-                         end: DateTime.new(2022, 1, 15),
-                         number: 5) }
+  subject do
+    pre_existing_rent
+    current_rent
+  end
+
   before(:each) do
     Timecop.freeze(DateTime.new(2022, 1, 1))
   end
@@ -15,40 +28,36 @@ RSpec.describe Rent, type: :model do
   after do
     Timecop.return
   end
-  it 'is valid when another rent will be taken after the end' do
-    Rent.create!(state: :approved, item: item, user: user,
-                 begin: DateTime.new(2022, 1, 16),
-                 end: DateTime.new(2022, 1, 17),
-                 number: 5)
-    expect(subject).to be_valid
+
+  context 'when the rent starts before the pre existing rent' do
+    let(:begin_date) { DateTime.new(2022, 1, 5) }
+    context 'and ends before the current rent' do
+      let(:end_date) { DateTime.new(2022, 1, 9) }
+      it { expect(subject).to be_valid }
+    end
+    context 'and ends on the current rent beginning date' do
+      let(:end_date) { DateTime.new(2022, 1, 10) }
+      it { expect(subject).to be_valid }
+    end
+    context 'and ends after the current_rent is take' do
+      let(:end_date) { DateTime.new(2022, 1, 11) }
+      it { expect(subject).not_to be_valid }
+    end
   end
 
-  it 'is invalid when another rent ends at the same day' do
-    Rent.create!(state: :approved, item: item, user: user,
-                begin: DateTime.new(2022, 1, 5),
-                end: DateTime.new(2022, 1, 10),
-                number: 5)
-    expect(subject).not_to be_valid
-  end
-  it 'is invalid when another rent begins at the same day' do
-    Rent.create!(state: :approved, item: item, user: user,
-                 begin: DateTime.new(2022, 1, 15),
-                 end: DateTime.new(2022, 1, 16),
-                 number: 5)
-    expect(subject).not_to be_valid
-  end
-  it 'is invaild when another rent is at the same time' do
-    Rent.create!(state: :approved, item: item, user: user,
-                 begin: DateTime.new(2022, 1, 10),
-                 end: DateTime.new(2022, 1, 15),
-                 number: 5)
-    expect(subject).not_to be_valid
-  end
-  it 'when another rent is begins before and ends after thisone' do
-    Rent.create!(state: :approved, item: item, user: user,
-                 begin: DateTime.new(2022, 1, 9),
-                 end: DateTime.new(2022, 1, 16),
-                 number: 5)
-    expect(subject).not_to be_valid
+  context 'when the rent ends after the pre existing rent' do
+    let(:end_date) { DateTime.new(2022, 1, 20) }
+    context 'and begins before the current rent is back' do
+      let(:begin_date) { DateTime.new(2022, 1, 14) }
+      it { expect(subject).not_to be_valid }
+    end
+    context 'and begins whe the current rent is back' do
+      let(:begin_date) { DateTime.new(2022, 1, 15) }
+      it { expect(subject).to be_valid }
+    end
+    context 'and begins after the current rent is back' do
+      let(:begin_date) { DateTime.new(2022, 1, 16) }
+      it { expect(subject).to be_valid }
+    end
   end
 end
